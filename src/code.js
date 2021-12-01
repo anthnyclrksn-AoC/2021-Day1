8ed9462a080c77d79ccdcb82d5b100a5e8d4dfa7
UP2024320 <86617698+UP2024320@users.noreply.github.com>
2021-12-01 17:07:52 +0000

    update code.js to have part 2

--- src/code.js
+++ src/code.js
@@ -1,5 +1,6 @@
 let fs = require('fs');
 
+//Part 1
 try {
     let data = fs.readFileSync('./src/depthMeasurement.txt', 'utf8');
     let splitData = data.toString().split('\n')
@@ -8,6 +9,28 @@ try {
         if(Number(splitData[i+1]) > Number(splitData[i])) {
             count = count + 1
         };
+    };
+    console.log(count)
+} catch (error) {
+    console.log(`Error: ${error.stack}`);
+};
+
+//part 2
+try {
+    let data = fs.readFileSync('./src/depthMeasurement.txt', 'utf8');
+    let splitData = data.toString().split('\n')
+    let count = 0
+    for(let i = 0; i < splitData.length; i++) {
+        let depthAdditionFirst = Number(splitData[i]) + Number(splitData[i+1]) + Number(splitData[i+2])
+        let depthAdditionSecond= Number(splitData[i+1]) + Number(splitData[i+2]) + Number(splitData[i+3])
+
+        if(depthAdditionFirst < depthAdditionSecond) {
+            count = count + 1
+            console.log(count)
+        }
+
+
+        console.log(splitData[i], splitData[i+1], splitData[i+2], depthAdditionFirst, depthAdditionSecond, count)
     };
     console.log(count)
 } catch (error) {
